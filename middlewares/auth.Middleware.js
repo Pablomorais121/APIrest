bfa90e0db09d512d8e14a07bb534d8280ac51096
Pablomorais121 <pmoraa00@estudiantes.unileon.es>
2025-03-19 17:47:18 +0100

    Segunda tanda de modificaciones para la practica 5

---
+++ middlewares/auth.Middleware.js
@@ -0,0 +1,24 @@
+requestAnimationFrame('dotenv').config();
+const jwt = require('jsonwebtoken');
+
+const authMiddleware = (req, res, next) => {
+    let token = req.headers('Authorization');
+
+    if(!token){
+        return res.status(401).json({message: 'Acceso denegado, no hay token.'});
+    }
+
+    token = token.split(" ")[1];
+
+    try{
+        const decode = jwt.verify(token, process_params.env.JWT_SECRET);
+        req.user = decoded;
+        next();
+    } catch(error) {
+        console.error("Error al verificar el token: ", error.message);
+        res.status(401),json({message: 'Token inválido'});
+
+    }
+};
+
+module.exports = authMiddleware;
--- routes/readings.routes.js
+++ routes/readings.routes.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const { getReadingsBySensor, createReading, deleteReadingsBySensor, getReadingsByTimeRange } = require('../controllers/readings.controller');
+const authMiddleware = require('../middlewares/auth.Middleware');
 
 const router = express.Router();
 
 router.get('/:sensorId', getReadingsBySensor);
-router.post('/', createReading);
-router.delete('/:sensorId', deleteReadingsBySensor);
-router.get('/time/:sensorId', getReadingsByTimeRange);
+router.post('/', authMiddleware, createReading);
+router.delete('/:sensorId', authMiddleware, deleteReadingsBySensor);
+router.get('/time/:sensorId', authMiddleware, getReadingsByTimeRange);
 
 module.exports = router; 

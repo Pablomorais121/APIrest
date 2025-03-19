c44ed04e223c8cf42d71fc9a52fe231e2b2b7c16
Pablomorais121 <pmoraa00@estudiantes.unileon.es>
2025-03-19 17:57:10 +0100

    modificado para pregunta 1 de practica 5

--- server.js
+++ server.js
@@ -2,8 +2,10 @@ const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const connectDB = require('./config/db');
+
 const sensorRoutes = require('./routes/sensors.routes');
 const readingRoutes = require('./routes/readings.routes');
+const authRoutes = require('./routes/auth.routes'); 
 
 var app = express();
 var port = 3000 || process.env.PORT;
@@ -14,7 +16,7 @@ connectDB();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cors({origin:['http://localhost:3000', 'http://192.168.1.X']}));
-app.use((req,res,next) => {
+app.use((res,next) => {
     res.setHeader('Content-Security-Policy', "default-src 'self'");
     res.setHeader('X-Content-Type-Options', 'nosniff');
     res.setHeader('X-Frame-Options', 'DENY');
@@ -24,6 +26,7 @@ app.disable('x-powered-by');
 
 app.use('/sensors',sensorRoutes);
 app.use('/readings', readingRoutes);
+app.use('/auth', authRoutes);
 
 app.get('/test', (req ,res) => res.json({msg: 'El API REST funciona!'}));
 
